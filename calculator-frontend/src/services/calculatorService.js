2cfbfe1f1feb724068a553b2c185986c3b994e82
james <beheramithun2021@gamil.com>
2026-08-02 01:03:54 +0530

    Connect frontend to Render backend

--- calculator-frontend/src/services/calculatorService.js
+++ calculator-frontend/src/services/calculatorService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://calculator-backend-hsho.onrender.com/api";
 
 const calculate = (data) => {
   return axios.post(`${API_URL}/calculate`, data);
